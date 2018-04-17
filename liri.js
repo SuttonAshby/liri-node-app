20cf368b23ccb20cfbd86d7664566decd1224dc8
Sutton Ashby <korim_fidelis@yahoo.com>
2018-04-17 00:33:02 -0400

    spotify and twitter added

--- liri.js
+++ liri.js
@@ -1,11 +1,12 @@
+var Spotify = require("node-spotify-api")
+var Twitter = require("twitter")
 var dotenv = require("dotenv").config();
 var keys = require("./keys.js")
 var inquirer = require("inquirer");
 var request = require("request");
 
-// var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
-
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
 var app = {
     initialize: function () {
@@ -16,16 +17,16 @@ var app = {
             name: "initial"
         }]).then(function (inquirerResponse) {
             if (inquirerResponse.initial === "Tweets") {
-                console.log(1)
+                app.getTwitter()
             } else if (inquirerResponse.initial === "Spotify") {
-                console.log(2)
+                app.spotifyPrompt()
             } else if (inquirerResponse.initial === "Movies") {
                 app.omdbPrompt()
             } else if (inquirerResponse.initial === "Do What it says") {
                 console.log(4)
-            } else if(inquirerResponse.initial === "Exit"){
-                console.log("==================="); 
-                console.log("Goodbye!!"); 
+            } else if (inquirerResponse.initial === "Exit") {
+                console.log("===================");
+                console.log("Goodbye!!");
                 console.log("===================");
             }
         })
@@ -36,7 +37,22 @@ var app = {
             message: "What song would you like to Spotify?",
             name: "song"
         }]).then(function (inquirerResponse) {
-
+            if (inquirerResponse.song === "") {
+                inquirerResponse.song = "The Sign Ace of Base"
+            }
+            spotify.search({ type: 'track', query: inquirerResponse.song, limit: 1 }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                // console.log(data.tracks.items[0]);
+                console.log("===================");
+                console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
+                console.log("Title: " + data.tracks.items[0].name);
+                console.log("Album: " + data.tracks.items[0].album.name);
+                console.log("Preview: " + data.tracks.items[0].preview_url);
+                console.log("===================");
+                app.againPrompt()
+            });
         })
     },
     omdbPrompt: function () {
@@ -45,7 +61,7 @@ var app = {
             message: "What Movie would you like to search for?",
             name: "movie"
         }]).then(function (inquirerResponse) {
-            if(inquirerResponse.movie === ""){
+            if (inquirerResponse.movie === "") {
                 inquirerResponse.movie = "Mr. Nobody"
             }
             request("http://www.omdbapi.com/?t=" + inquirerResponse.movie + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
@@ -65,18 +81,34 @@ var app = {
             });
         })
     },
-    againPrompt: function(){
+    getTwitter: function () {
+        var params = { screen_name: 'Bot_tanical' };
+        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+            if (!error) {
+                // console.log(tweets);
+                // console.log(response);
+                tweets.forEach(function(element) {
+                    console.log("===================");
+                    console.log("Created At: " + element.created_at);
+                    console.log("Message: " + element.text);
+                    console.log("===================");
+                });
+                app.againPrompt()
+            }
+        });
+    },
+    againPrompt: function () {
         inquirer.prompt([{
             type: "list",
             message: "Would you like to do something else?",
             choices: ["Yes", "No"],
             name: "again"
-        }]).then(function (inquirerResponse){
-            if(inquirerResponse.again === "Yes"){
+        }]).then(function (inquirerResponse) {
+            if (inquirerResponse.again === "Yes") {
                 app.initialize()
             } else {
-                console.log("==================="); 
-                console.log("Goodbye!!"); 
+                console.log("===================");
+                console.log("Goodbye!!");
                 console.log("===================");
             }
         });
